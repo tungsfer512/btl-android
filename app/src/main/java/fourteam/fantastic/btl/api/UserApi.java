b413caaae919697c2fb890491ed3dd8acb2bfc6b
Bui Van Tung <tungbv5122001@gmail.com>
2023-04-26 00:51:14 +0700

    add cart activity

--- app/src/main/java/fourteam/fantastic/btl/api/UserApi.java
+++ app/src/main/java/fourteam/fantastic/btl/api/UserApi.java
@@ -6,16 +6,20 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import fourteam.fantastic.btl.R;
+import fourteam.fantastic.btl.RequestBody.AddressRequestBody;
 import fourteam.fantastic.btl.model.User;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 
 public interface UserApi {
@@ -50,4 +54,12 @@ public interface UserApi {
     @POST("/users/auth/register")
     Call<Object> postRegister(@Field("username") String username, @Field("password") String password, @Field("first_name") String firstname, @Field("last_name") String lastname, @Field("email") String email);
 
+    @POST("/addresses")
+    Call<Object> addAddress(@Body AddressRequestBody addressRequestBody);
+
+    @GET("/addresses")
+    Call<Object> getAllAddresses(@Query("user") Integer user);
+
+    @GET("addresses/{id}")
+    Call<Object> getAddressById(@Path("id") Integer id);
 }
